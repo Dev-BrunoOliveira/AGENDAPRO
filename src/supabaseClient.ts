fbb34637f907420c3007493c01dac6b18e962394
Bruno Oliveira <brunooliver2015@outlook.com>
2026-05-13 14:41:26 -0300

    supabase e google integrados

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,7 +1,7 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 
-// Certifique-se de que a URL começa com https://
-const supabaseUrl = 'https://xyz.supabase.co' 
-const supabaseAnonKey = 'sua-chave-anon-longa-aqui'
+const supabaseUrl = "https://kokuthzksiygmpmrmqss.supabase.co";
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtva3V0aHprc2l5Z21wbXJtcXNzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2NzYwMTIsImV4cCI6MjA5NDI1MjAxMn0.SIV8fTGeYsJY9M1U_taPPC3mnl-mAYvAf30wnqu5GHk";
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
