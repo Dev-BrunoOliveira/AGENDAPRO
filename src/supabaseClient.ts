adcaa7e86e7f60d6b8d1d21d00956174a1bd8cce
Bruno Oliveira <brunooliver2015@outlook.com>
2026-06-26 21:48:16 -0300

    estrutura home

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://kokuthzksiygmpmrmqss.supabase.co";
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtva3V0aHprc2l5Z21wbXJtcXNzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2NzYwMTIsImV4cCI6MjA5NDI1MjAxMn0.SIV8fTGeYsJY9M1U_taPPC3mnl-mAYvAf30wnqu5GHk";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || "https://kokuthzksiygmpmrmqss.supabase.co";
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtva3V0aHprc2l5Z21wbXJtcXNzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2NzYwMTIsImV4cCI6MjA5NDI1MjAxMn0.SIV8fTGeYsJY9M1U_taPPC3mnl-mAYvAf30wnqu5GHk";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
